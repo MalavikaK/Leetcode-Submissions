9b6aa8564977e487e6d0a99dbd4529a9488b459e
Malavika Krishnan <30767136+MalavikaK@users.noreply.github.com>
2022-06-30 19:51:45 -0400

    Time: 15 ms (45.60%), Space: 71.4 MB (22.75%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.java
+++ 217-contains-duplicate/217-contains-duplicate.java
@@ -1,6 +1,6 @@
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-        Set<Integer> set = new HashSet<>();
+        Set<Integer> set = new HashSet<>(); //to store the elements that occured once
         for(int i=0; i<nums.length; i++){
             int x = nums[i];
             if(set.contains(x))
@@ -10,3 +10,6 @@ class Solution {
         return false;
     }
 }
+
+//logic
+//Use a HashSet to store the elements in the array that occured only once. If the same number is encountered again, return true. Else return false. Time complexity is O(n) and space complexity is O(1).
