0a80dbaa990a276cfddc1a40cef8181987f894e6
Malavika Krishnan <30767136+MalavikaK@users.noreply.github.com>
2022-07-19 07:12:30 -0400

    Time: 4 ms (76.56%), Space: 57.3 MB (52.55%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.java
+++ 200-number-of-islands/200-number-of-islands.java
@@ -1,23 +1,24 @@
 class Solution {
-    public int numIslands(char[][] grid) {       
-        int count = 0;        
-        for (int i = 0; i < grid.length; i++){
-            for(int j = 0; j < grid[i].length; j++){
+    public int numIslands(char[][] grid) {
+        int count = 0;
+        for(int i=0; i<grid.length; i++){
+            for(int j=0; j<grid[i].length; j++){
                 if(grid[i][j] == '1'){
-                    count += 1;
-                    callBFS(grid, i, j);
+                    callBFS(grid,i,j);
+                    count +=1;
                 }
             }
         }
-        return count;        
-    }    
-    public void callBFS(char[][] grid, int i, int j){
-        if(i < 0 || i >= grid.length || j < 0 || j >= grid[i].length || grid[i][j] == '0')
-            return;
-         grid[i][j] = '0';
-         callBFS(grid, i+1, j);
-         callBFS(grid, i-1, j);
-         callBFS(grid, i, j+1);
-         callBFS(grid, i, j-1);        
+        return count;
+    }
+        public void callBFS(char[][] grid, int i, int j){
+            if(i < 0 || i >= grid.length || j < 0 || j >= grid[i].length || grid[i][j] == '0')
+                return;
+                grid[i][j] = '0';
+                callBFS(grid, i+1, j);
+                callBFS(grid, i-1, j);
+                callBFS(grid, i, j+1);
+                callBFS(grid, i, j-1);
+        
     }
 }
