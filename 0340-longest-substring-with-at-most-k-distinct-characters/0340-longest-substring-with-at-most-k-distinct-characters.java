e19691242a888f1704bae94b03e1cd6fe49ae600
Malavika Krishnan <30767136+MalavikaK@users.noreply.github.com>
2022-10-19 08:43:34 -0400

    Time: 22 ms (35.43%), Space: 45.1 MB (51.37%) - LeetHub

---
+++ 0340-longest-substring-with-at-most-k-distinct-characters/0340-longest-substring-with-at-most-k-distinct-characters.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int lengthOfLongestSubstringKDistinct(String s, int k) {
+        int start = 0;
+        int max_length = Integer.MIN_VALUE;
+      
+        HashMap<Character,Integer> map = new HashMap<>();
+        
+        for(int end = 0; end < s.length(); end++){
+                 map.put(s.charAt(end), end);
+            if(map.size() >= k + 1){
+                map.remove(s.charAt(start), start);
+                start++;
+            }
+           max_length = Math.max(max_length, end - start + 1);
+        }
+            return max_length;
+    }
+ }
+
+// dynamic sliding window problem
+//s = "eceba", k = 2
