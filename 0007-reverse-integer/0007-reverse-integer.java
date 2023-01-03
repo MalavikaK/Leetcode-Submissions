75b3ca8cee0151c9822e18c965c5865e068d6fc7
Malavika Krishnan <30767136+MalavikaK@users.noreply.github.com>
2023-01-03 16:13:02 -0500

    Time: 2 ms (67.92%), Space: 41.9 MB (12.94%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.java
+++ 0007-reverse-integer/0007-reverse-integer.java
@@ -1,33 +1,34 @@
-// class Solution {
-//     public int reverse(int x) {
+class Solution {
+    public int reverse(int x) {
         
-//         int rev = 0;
-//         while(x != 0 ){
-//             int rem = x % 10;
-//             rev = rev * 10 + rem;
-//             x = x / 10;
-//         }
-//         if(rev < Integer.MIN_VALUE && rev >= Integer.MAX_VALUE){
-//             return 0;
-//         }
-//         else{
-//             return rev;
-//         }
-        
-//     }
-// }
-class Solution{
-public int reverse(int x) {
-        long res = 0;
-        while (x != 0) {
-            res = res * 10 + x % 10;
+        long rev = 0;
+        while(x != 0 ){
+            int rem = x % 10;
+            rev = rev * 10 + rem;
             x = x / 10;
         }
-        
-        if (res < Integer.MIN_VALUE || res > Integer.MAX_VALUE) {
+        if(rev < Integer.MIN_VALUE || rev > Integer.MAX_VALUE){
             return 0;
-        } else {
-            return (int)res;
         }
+        else{
+            return (int)rev;
+        }
+        
     }
 }
+
+// class Solution{
+// public int reverse(int x) {
+//         long res = 0;
+//         while (x != 0) {
+//             res = res * 10 + x % 10;
+//             x = x / 10;
+//         }
+        
+//         if (res < Integer.MIN_VALUE || res > Integer.MAX_VALUE) {
+//             return 0;
+//         } else {
+//             return (int)res;
+//         }
+//     }
+// }
