80e2d9b5b5131509e71e26198221d9eeb4bcf694
Malavika Krishnan <30767136+MalavikaK@users.noreply.github.com>
2025-07-05 09:18:00 -0400

    Time: 327 ms (43.54%), Space: 32 MB (43.36%) - LeetHub

---
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.py
@@ -0,0 +1,29 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        n=len(prices)
+
+        # step 1 to find the profit from day 0 to i
+
+        left_profit = [0] * n
+        min_price = prices[0]
+        for i in range (1,len(prices)):
+            min_price= min(min_price,prices[i])
+            left_profit[i] = max(left_profit[i-1],prices[i]-min_price)
+
+        # step 2 to find profit from day i to n-1
+
+        right_profit = [0] * n
+        max_price = prices[n-1]
+        for i in range (n-2,-1,-1):
+            max_price = max(max_price,prices[i])
+            right_profit[i] =max(right_profit[i+1], max_price-prices[i])
+
+        # step 3 is to find total profit from two transactions 
+
+        max_total_profit = 0
+        for i in range(n):
+            total_profit = left_profit[i] + (right_profit[i+1] if i+1 < n else 0)
+            max_total_profit = max(total_profit, max_total_profit)
+        return max_total_profit
+          
+        
