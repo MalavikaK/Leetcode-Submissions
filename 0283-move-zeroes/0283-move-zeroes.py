049e6fd97190f656e68187b7063b30257d119aed
Malavika Krishnan <30767136+MalavikaK@users.noreply.github.com>
2025-07-06 18:32:23 -0400

    Time: 3 ms (80.32%), Space: 19 MB (31.13%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -0,0 +1,18 @@
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        insert_pos = 0
+        for i in range (len(nums)):
+
+            if(nums[i] != 0):
+                # for every i num in nums array, if there are non zero elements,  
+                # move the elemnt to insert_pos location 
+                nums[insert_pos] = nums[i]
+                insert_pos += 1
+        
+        for i in range (insert_pos, len(nums)):
+            nums[i] = 0
+        
+        return nums
