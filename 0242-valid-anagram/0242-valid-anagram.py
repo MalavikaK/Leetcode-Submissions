3564db21469db7f03f9100317e108a428e6c4aa5
Malavika Krishnan <30767136+MalavikaK@users.noreply.github.com>
2025-07-20 09:15:54 -0400

    Time: 3 ms (98.35%), Space: 17.8 MB (64.14%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,18 +1,4 @@
+from collections import Counter
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        if (len(s) != len(t)):
-            return False
-    
-        hashmap1 = {}
-        for ch in s:
-            hashmap1[ch] = hashmap1.get(ch,0) + 1
-
-        for ch in t:
-            if ch not in hashmap1: 
-                return False
-            hashmap1[ch] -= 1
-
-            if hashmap1[ch] < 0 :
-                return False
-
-        return True
+        return Counter(s) == Counter(t)
